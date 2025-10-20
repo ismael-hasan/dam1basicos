72df1fe9d34b15be78e5fdb523b8099104b1fde0
Ismael Hasan Romero <ismael.hasan.romero@gmail.com>
2025-10-20 13:43:00 +0200

    Solucionado el ejercicio de la máquina de refrescos

--- src/MaquinaRefrescos.java
+++ src/MaquinaRefrescos.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MaquinaRefrescos {
     public static void main (String [] args) {
 /*        Pide al usuario su saldo y el precio del refresco.
@@ -6,5 +8,18 @@ public class MaquinaRefrescos {
                 Permite repetir la compra con un bucle while hasta que el usuario introduzca saldo suficiente.
 
  */
+        Scanner lector = new Scanner(System.in);
+        System.out.println("¿Cuanto dinero tienes");
+        int dinero = lector.nextInt();
+        System.out.println("¿Cuanto vale el refresco que quieres comprar?");
+        int precioRefresco = lector.nextInt();
+
+        // repetiremos el bucle hasta que el usuario se quede sin dinero.
+        while (dinero>=precioRefresco) {
+            System.out.println("Has comprado el refresco.");
+            dinero = dinero - precioRefresco;
+            System.out.println("Te quedan: " + dinero);
+        }
+        System.out.println("Te quedan " + dinero + " euros. Ya no puedes comprar más refrescos.");
     }
 }
